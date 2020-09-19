db2c8a96e786c734580f3151d25b6d08518ed984
Andhikananda <akun.andhika.nanda@gmail.com>
2020-09-19 13:40:31 +0700

    Update apalah1.js

--- apalah1.js
+++ apalah1.js
@@ -3,20 +3,20 @@ function jquery_onload() {
         'scrollTop': $('#welcome').offset().top
     }, 0), $('html, body').animate({
         'scrollLeft': $('#welcome').offset().left
-    }, 0), $(document).on('click', 'a[href*="#about"]', function (_0x4ae5cc) {
-        _0x4ae5cc.preventDefault(), $('html, body').animate({
+    }, 0), $(document).on('click', 'a[href*="#about"]', function (h) {
+        h.preventDefault(), $('html, body').animate({
             'scrollLeft': $('#about').offset().left
         }, 400);
-    }), $(document).on('click', 'a[href*="#loop"]', function (_0x2c7597) {
-        _0x2c7597.preventDefault(), $('html, body').animate({
+    }), $(document).on('click', 'a[href*="#loop"]', function (c) {
+        c.preventDefault(), $('html, body').animate({
             'scrollTop': $('#loop').offset().top
         }, 200);
-    }), $(document).on('click', 'a[href*="#contact"]', function (_0x5bb07e) {
-        _0x5bb07e.preventDefault(), $('html, body').animate({
+    }), $(document).on('click', 'a[href*="#contact"]', function (b) {
+        b.preventDefault(), $('html, body').animate({
             'scrollLeft': $('#contact').offset().left
         }, 400);
-    }), $(document).on('click', 'a[href*="#welcome"]', function (_0x3866dd) {
-        _0x3866dd.preventDefault(), $('html, body').animate({
+    }), $(document).on('click', 'a[href*="#welcome"]', function (e) {
+        e.preventDefault(), $('html, body').animate({
             'scrollTop': $('#welcome').offset().top
         }, 200);
     }), lazyload();
@@ -39,20 +39,17 @@ function lazyload() {
         }));
     }), $(window).on('scroll', function () {
         $('[data-src]').each(function () {
-            var _0x344fa2 = $(this),
-                _0x470d63 = $(this).attr('data-src'),
-                _0x35fa31 = $(this).prop('tagName').toLowerCase(),
-                _0x4a6680 = $(this).offset().top;
-            if (_0x470d63) {
-                var _0x1c588d = $(window).height(),
-                    _0x5a9c18 = $(window).scrollTop();
-                _0x5a9c18 + _0x1c588d > _0x4a6680 && (_0x35fa31 == 'img' || _0x35fa31 == 'iframe' ? (_0x344fa2.attr('src', _0x470d63), _0x344fa2.on('load', function () {
-                    _0x344fa2.addClass('loaded'), setTimeout(function () {
-                        _0x344fa2.removeClass('loaded').removeAttr('data-src');
+            var ad = $(this).offset().top;
+            if ($(this).attr('data-src')) {
+                var af = $(window).height(),
+                    ae = $(window).scrollTop();
+                ae + af > ad && ($(this).prop('tagName').toLowerCase() == 'img' || $(this).prop('tagName').toLowerCase() == 'iframe' ? ($(this).attr('src', $(this).attr('data-src')), $(this).on('load', function () {
+                    $(this).addClass('loaded'), setTimeout(function () {
+                        $(this).removeClass('loaded').removeAttr('data-src');
                     }, 100);
-                })) : $('<img/>').attr('src', _0x470d63).on('load', function () {
-                    $(this).remove(), _0x344fa2.css('background-image', 'url(' + _0x470d63 + ')'), _0x344fa2.addClass('loaded'), setTimeout(function () {
-                        _0x344fa2.removeClass('loaded').removeAttr('data-src');
+                })) : $('<img/>').attr('src', $(this).attr('data-src')).on('load', function () {
+                    $(this).remove(), $(this).css('background-image', 'url(' + $(this).attr('data-src') + ')'), $(this).addClass('loaded'), setTimeout(function () {
+                        $(this).removeClass('loaded').removeAttr('data-src');
                     }, 100);
                 }));
             }
